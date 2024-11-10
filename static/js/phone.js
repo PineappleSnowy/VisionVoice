2b89241d8b477f9dba57be47c05a4bc2d7296d74
StarsAC <starsacpublic@163.com>
2024-11-10 16:46:21 +0800

    feat(phone): 初步添加音频波形显示功能，修复前后相机切换功能，更换 socket.io 的cdn为字节公共cdn
    - 在页面中添加 canvas 元素，用于显示音频波形
    - 使用 Vudio 库实现音频波形的实时渲染
    - 优化摄像头相关功能，添加错误处理和用户提示
    - 调整 CSS 样式，以适应新的音频波形显示功能

--- static/js/phone.js
+++ static/js/phone.js
@@ -4,6 +4,8 @@ const html = document.querySelector('html');
 html.style.fontSize = window.innerWidth * 100 / 412 + 'px';
 
 let stream;
+let audioStream;
+let vudio;
 const goBack = document.querySelector('.goBack');
 const toggleCamera = document.querySelector('.toggleCamera');
 const openCamera = document.querySelector('.openCamera');
@@ -11,6 +13,8 @@ const hangUp = document.querySelector('.hangUp');
 const container = document.querySelector('.container');
 const video = document.querySelector('video');
 const img = document.querySelector('img');
+const audio = document.querySelector('#audio');
+const waveShape = document.querySelector('#waveShape');
 let videoChat = false;
 let isFrontCamera = false;
 
@@ -26,9 +30,19 @@ openCamera.addEventListener('click', async () => {
     try {
         videoChat = !videoChat;
         if (videoChat) {
-            stream = await navigator.mediaDevices.getUserMedia({ video: {facingMode: "environment"}, audio: false });
-            video.srcObject = stream;
-            video.play();
+            try {
+                stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" }, audio: false });
+                video.srcObject = stream;
+            }catch(err) {
+                if (err.name === 'NotAllowedError') {
+                    alert('请允许访问您的摄像头！');
+                } else if (err.name === 'NotFoundError') {
+                    alert('未找到可用的摄像头！');
+                } else {
+                    alert('发生错误: ' + err.message);
+                }
+            }
+
             video.style.display = 'block';
             container.classList.add('shifted');
             toggleCamera.style.display = 'block';
@@ -36,6 +50,9 @@ openCamera.addEventListener('click', async () => {
             goBack.style.color = 'white';
             toggleCamera.style.color = 'white';
         } else {
+            stream.getTracks().forEach((track) => {
+                track.stop();
+            });
             video.srcObject = null;
             video.style.display = 'null';
             container.classList.remove('shifted');
@@ -43,25 +60,65 @@ openCamera.addEventListener('click', async () => {
             img.style.display = 'block';
             goBack.style.color = 'black';
         }
-    }catch (err) {
+    } catch (err) {
         console.log(err);
     }
 
 });
 
-toggleCamera.addEventListener('click',async ()=>{
+toggleCamera.addEventListener('click', async () => {
     if (stream) {
-        video.srcObject = null;
-        stream.getTracks().forEach(track => track.stop());
+        
         isFrontCamera = !isFrontCamera;
-        const constraints = { video: { facingMode: isFrontCamera ? 'user' : 'environment' } };
-        console.log(constraints);
-        try{
-            stream = await navigator.mediaDevices.getUserMedia(constraints);
-            video.srcObject = stream;
-        }catch(err){
-            alert(err);
+        // Stop previous stream
+        stream.getTracks().forEach((track) => {
+            track.stop();
+        });
+        if(isFrontCamera){
+            try {
+                stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'user' }, audio: false});
+                video.srcObject = stream;
+            } catch (err) {
+                alert(err);
+            }
+        }else{
+            try {
+                stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
+                video.srcObject = stream;
+            } catch (err) {
+                alert(err);
+            }
         }
         
     }
 });
+
+window.onload = async () => {
+    try {
+        audioStream = await navigator.mediaDevices.getUserMedia({ audio: true });
+
+        // audio.srcObject = audioStream;
+        vudio = new window.Vudio(audioStream, waveShape, {
+            effect: 'waveform', // 当前只有'waveform'这一个效果，还有lighting效果
+            accuracy: 16, // 精度,实际表现为波形柱的个数，范围16-16348，必须为2的N次方
+            width: window.innerWidth * 100 / 412 * 1.5, // canvas宽度，会覆盖canvas标签中定义的宽度
+            height: window.innerWidth * 100 / 412 * 1, // canvas高度，会覆盖canvas标签中定义的高度
+            waveform: {
+                maxHeight: 80, // 最大波形高度
+                minHeight: 0, // 最小波形高度
+                spacing: 5, // 波形间隔
+                color: '#000', // 波形颜色，可以传入数组以生成渐变色
+                shadowBlur: 0, // 阴影模糊半径
+                shadowColor: '#f00', // 阴影颜色
+                fadeSide: true, // 渐隐两端
+                horizontalAlign: 'center', // 水平对齐方式，left/center/right
+                verticalAlign: 'middle', // 垂直对齐方式 top/middle/bottom
+                radius: 20 //添加属性，设置圆角
+            }
+        
+        });
+        vudio.dance();
+    }catch(err) {
+        alert(err);
+    }
+};
