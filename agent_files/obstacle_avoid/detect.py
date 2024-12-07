3fc2bbee92c28bb3648e49ffcff0990e0aadf85b
Hex671 <2609506645@qq.com>
2024-12-07 23:16:13 +0800

    优化代码

--- agent_files/obstacle_avoid/detect.py
+++ agent_files/obstacle_avoid/detect.py
@@ -1,7 +1,8 @@
 from ultralytics import YOLO
 import cv2
 
-model = YOLO("yolo11n.yaml").load("./agent_files/obstacle_avoid/best.pt")  # 加载模型
+# model = YOLO("yolo11n.yaml").load("./agent_files/obstacle_avoid/best.pt")  # 加载模型
+model = YOLO("./agent_files/obstacle_avoid/yolo11x-seg.pt")
 
 # YOLO检测种类字典
 yolo_classes = {
@@ -194,14 +195,14 @@ def detect(frame):
         else:
             label_0 = "unknown"
             label = "未知障碍物"
-        label_1 = label_0 + "f{i}"
+        label_1 = label_0 + f"{i}"
         if confidence > confidence_threshold:
             p = y + h / 2  # 像素坐标
             distance_real = -2.08054369e-12*p**4 + 6.25478630e-09*p**3 - 5.88280567e-06*p**2 + 1.07600182e-03*p + 9.74917750e-01
             distance_dic[label_1] = distance_real
             distance_dic_all[label_1] = [label, distance_real, x/width, y/height]
     label_min, distance_min = min(distance_dic.items(), key=lambda x: x[1])
-    obstacle_info.append({"label": distance_dic_all[label_min][0].item(), "distant": distance_dic_all[label_min][1].item(),
+    obstacle_info.append({"label": distance_dic_all[label_min][0], "distant": distance_dic_all[label_min][1].item(),
                           "left":distance_dic_all[label_min][2].item(),"top": distance_dic_all[label_min][3].item()})
     return obstacle_info
 
