8e32376d0e3de06fc51b1eddbe4eb9b97c51554d
Hex671 <2609506645@qq.com>
2024-12-07 22:43:26 +0800

    优化返回信息为中心点所占百分比，解决多个同类物品数据存取冲突问题

--- agent_files/obstacle_avoid/detect.py
+++ agent_files/obstacle_avoid/detect.py
@@ -179,6 +179,7 @@ def detect(frame):
     return:
         返回标签、距离、位置信息
     """
+    height, width = frame.shape[:2]
     confidence_threshold = 0.35  # 设置置信度阈值
     results = model(frame)
     distance_dic = dict()  #用于比较，获取最近障碍物标签以及距离
@@ -193,15 +194,15 @@ def detect(frame):
         else:
             label_0 = "unknown"
             label = "未知障碍物"
+        label_1 = label_0 + "f{i}"
         if confidence > confidence_threshold:
             p = y + h / 2  # 像素坐标
             distance_real = -2.08054369e-12*p**4 + 6.25478630e-09*p**3 - 5.88280567e-06*p**2 + 1.07600182e-03*p + 9.74917750e-01
-            distance_dic[label] = distance_real
-            distance_dic_all[label] = [y + h / 2, w, h]
+            distance_dic[label_1] = distance_real
+            distance_dic_all[label_1] = [label, distance_real, x/width, y/height]
     label_min, distance_min = min(distance_dic.items(), key=lambda x: x[1])
-    obstacle_info.append({"label": label_min, "distant": distance_min.item(),
-                          "location": {"tl": distance_dic_all[label_min][0].item(), "w": distance_dic_all[label_min][1].item(), "h": distance_dic_all[label_min][2].item()}})
-    # print(f"{label_min}距离{distance_min}:{distance_dic_all[label_min][0]}:{distance_dic_all[label_min][1]}:{distance_dic_all[label_min][2]}")
+    obstacle_info.append({"label": distance_dic_all[label_min][0].item(), "distant": distance_dic_all[label_min][1].item(),
+                          "left":distance_dic_all[label_min][2].item(),"top": distance_dic_all[label_min][3].item()})
     return obstacle_info
 
 def obstacle_avoid_realize(path):
